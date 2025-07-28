00d200d96affa7e3b12bb8f3485ea686ffbbf727
shimizu.kenji <shimizu.kenji@noncore.net>
2025-07-28 17:28:02 +0900

    index.htmlにスタイルシートを追加し、vite.config.tsにパスエイリアスを設定しました。また、新たにmain.cssファイルを作成し、ドラッグや選択に関するスタイルを定義しました。

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  resolve: {
+    alias: { '@/': '/src/' },
+  },
   plugins: [
     tanstackRouter({
       target: 'react',
