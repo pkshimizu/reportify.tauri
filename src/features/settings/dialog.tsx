f2cc7df81e03c9a8fa52c004105e21ad083673c6
shimizu.kenji <shimizu.kenji@noncore.net>
2025-09-20 13:31:13 +0900

    設定ダイアログに新しい`GeneralPanel`および`GitHubPanel`コンポーネントを追加し、選択状態に応じて表示を切り替える機能を実装しました。また、ダイアログに閉じるボタンを追加しました。

--- src/features/settings/dialog.tsx
+++ src/features/settings/dialog.tsx
@@ -2,9 +2,12 @@ import RList from '@/components/display/list';
 import RDialog from '@/components/feedback/dialog';
 import RGitHubIcon from '@/components/icons/github';
 import RSettingIcon from '@/components/icons/setting';
+import RButton from '@/components/input/button';
 import { RColumn } from '@/components/layout/flex-box';
 import RGrid from '@/components/layout/grid';
 import { useState } from 'react';
+import GeneralPanel from './general-panel';
+import GitHubPanel from './github-panel';
 
 interface Props {
   open: boolean;
@@ -14,9 +17,14 @@ interface Props {
 export default function SettingsDialog(props: Props) {
   const [selected, setSelected] = useState<string>('general');
   return (
-    <RDialog onClose={props.onClose} open={props.open} size='md'>
+    <RDialog
+      onClose={props.onClose}
+      open={props.open}
+      size='md'
+      actions={<RButton onClick={props.onClose}>Close</RButton>}
+    >
       <RGrid columns={['240px', '1fr']} gap={2}>
-        <RColumn height={640}>
+        <RColumn height={480}>
           <RList
             items={[
               {
@@ -36,6 +44,8 @@ export default function SettingsDialog(props: Props) {
             ]}
           />
         </RColumn>
+        {selected === 'general' && <GeneralPanel />}
+        {selected === 'github' && <GitHubPanel />}
       </RGrid>
     </RDialog>
   );
---
+++ src/features/settings/general-panel.tsx
@@ -0,0 +1,3 @@
+export default function GeneralPanel() {
+  return <div>GeneralPanel</div>;
+}
